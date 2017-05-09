57efc8dc49bb876294a051ef4309d9fa0ea1401a
balavihar <pbalavihar@gmail.com>
2017-05-09 22:11:08 +0100

    Added new module for core java

---
+++ examples/core-java/src/main/java/com/core/example/patterns/MatcherPatterns.java
@@ -0,0 +1,33 @@
+package com.core.example.patterns;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by 609072804 on 09/05/2017.
+ */
+public class MatcherPatterns {
+
+    public static final String REGEX_PATTERN = "[a-zA-Z0-9_一-龯]+";//"[zenkaku 全角]";//"[a-zA-Z0-9_]+";
+
+    private static boolean matchesRegExp(final String regExp, String str) {
+
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(matchesRegExp(REGEX_PATTERN, "$&£*$£"));
+
+        System.out.println(matchesRegExp(REGEX_PATTERN, "sas12_sdad67"));
+
+        System.out.println(matchesRegExp(REGEX_PATTERN, "漢字"));
+
+        System.out.println("test "+matchesRegExp(REGEX_PATTERN, "テスト"));
+
+        System.out.println(matchesRegExp(REGEX_PATTERN, "角"));
+
+        System.out.println(matchesRegExp(REGEX_PATTERN, "漢字日本語文字言語言葉"));
+    }
+}
